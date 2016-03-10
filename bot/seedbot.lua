d4dbeb935e891958810916e4d5e5f88657b9db4c
parsaalemi <pa.alemi81@gmail.com>
2016-03-10 17:34:52 +0000

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {105831687},--Sudo users
+    sudo_users = {105831687,209846255},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
